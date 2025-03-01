9aebd6abe356539062329ac77cd97e61502a8062
Zuli Dobariya <zulidobariya24@gmail.com>
2025-03-01 15:08:58 +0530

    equipment routes

--- AgriSutra/server/server.js
+++ AgriSutra/server/server.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
 const newsRoutes = require("./routes/news");
 const profileRoute = require("./routes/profile");
+const categoriesRoutes = require("./routes/categories");
+const equipmentRoutes = require("./routes/equipment"); // Import the equipment routes
+const usersRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -12,11 +15,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/news", newsRoutes);
 app.use("/api/profile", profileRoute);
+app.use("/api/equipment", equipmentRoutes);
+app.use("/api/categories", categoriesRoutes);
+app.use("/api/users", usersRoute);
 
 const { Pool } = require("pg");
 const pool = new Pool({
